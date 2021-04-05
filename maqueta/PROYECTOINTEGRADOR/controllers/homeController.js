277de8029ddbe31552a219464a33aac9e2ab9fef
rochijacob <rochijacob@gmail.com>
2021-04-05 20:22:38 -0300

    proyecto comenzado
    uni algunas imagenes,
    Hice todos los stylesheets
    Rutee "register"
    Hay que seguir acomodando todo para poder empezar a trabajar con cosas mas dinamicas!

--- maqueta/PROYECTOINTEGRADOR/controllers/homeController.js
+++ maqueta/PROYECTOINTEGRADOR/controllers/homeController.js
@@ -2,6 +2,9 @@ let controller = {
     index: (req, res) => {
         res.render('index', {title: 'Mercado Liebre'})
     },
+    register: (req, res) => {
+        res.render('register')
+    }
 
 }
 
--- maqueta/PROYECTOINTEGRADOR/routes/index.js
+++ maqueta/PROYECTOINTEGRADOR/routes/index.js
@@ -5,5 +5,7 @@ let homeController = require ('../controllers/homeController');
 /* GET home page. */
 router.get('/', homeController.index);
 
+router.get('/register', homeController.register);
+
 
 module.exports = router;
