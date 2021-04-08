7b1f4cf056080bd1209b7d9e60aa0bdbd918d169
rochijacob <rochijacob@gmail.com>
2021-04-08 19:12:27 -0300

    cambioss

--- maqueta/PROYECTOINTEGRADOR/controllers/homeController.js
+++ maqueta/PROYECTOINTEGRADOR/controllers/homeController.js
@@ -1,9 +1,12 @@
 let controllerLogin = {
     index: (req, res) => {
-        res.render('login', {title: 'Mercado Liebre'})
+        res.render('index', {title: 'Mercado Liebre'})
     },
     register: (req, res) => {
         res.render('register')
+    },
+    login: (req, res) => {
+        res.render('login', {title: 'Mercado Liebre'})
     }
 
 }
--- maqueta/PROYECTOINTEGRADOR/routes/index.js
+++ maqueta/PROYECTOINTEGRADOR/routes/index.js
@@ -5,7 +5,9 @@ let homeController = require ('../controllers/homeController');
 /* GET home page. */
 router.get('/', homeController.index);
 
-router.get('/register', homeController.register);
+router.get('/register', homeController.register); // esto crea el siguiente link "/register/"
+
+router.get('/login', homeController.login);
 
 
 module.exports = router;
