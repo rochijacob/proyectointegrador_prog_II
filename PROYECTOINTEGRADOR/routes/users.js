dc545ed9cfd5de66eb939b3f59225c6b9fc41b23
natalyyemal <nyemal@udesa.edu.ar>
2021-07-05 11:45:40 -0300

    up

--- PROYECTOINTEGRADOR/routes/users.js
+++ PROYECTOINTEGRADOR/routes/users.js
@@ -13,15 +13,15 @@ const path = require('path');
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images/users')
+    destination: (req, file, cb) => { //donde almaceno el archivo
+        cb(null, 'public/images/users') //ruta donde almaceno el archivo
     },
-    filename: (req, file, cb) => {
+    filename: (req, file, cb) => { //como se va a guardar
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     },
 });
 
-const upload = multer({
+const upload = multer({ //generico, variable upload q contenga multer
     storage: storage
 })
 
